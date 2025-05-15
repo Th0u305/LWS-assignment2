7a8670cbedb31cce58bea9c0a8e1142480d6e278
Th0u305 <ahnaf4411@gmail.com>
2025-05-15 16:40:34 +0600

    img loading error

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
     <nav className="bg-navbg rounded-full mt-4 px-8 py-3 flex justify-between items-center">
       <div className="flex items-center ">
         <div className="text-primary mr-2">
-          <img src="../../../public/assets/logo.svg" />
+          <img src="../../assets/logo.svg" />
         </div>
         <h1 className="text-2xl font-bold">
           <span className="text-primary">Dine</span>Out
         </h1>
       </div>
       <div className="flex items-center">
-        <img src="../../../public/assets/user-icon.svg" className="h-10" />
+        <img src="../../assets/user-icon.svg" className="h-10" />
       </div>
     </nav>
   );
