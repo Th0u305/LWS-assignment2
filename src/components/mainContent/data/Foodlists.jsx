07ecd947369cb68aad8854580e50ce76a1fbe3b4
Th0u305 <ahnaf4411@gmail.com>
2025-05-16 04:11:42 +0600

    image error

--- src/components/mainContent/data/Foodlists.jsx
+++ src/components/mainContent/data/Foodlists.jsx
@@ -1,28 +1,28 @@
 export const foodList = [
   {
     id: 0,
-    img: "../../assets/hamburger.svg",
+    img: "/assets/hamburger.svg",
     name: "Hamburger",
     price: 300,
     addItem: false,
   },
   {
     id: 1,
-    img: "../../assets/chicken.svg",
+    img: "/assets/chicken.svg",
     name: "Chicken Nuggets",
     price: 300,
     addItem: false,
   },
   {
     id: 2,
-    img: "../../assets/submarine.svg",
+    img: "/assets/submarine.svg",
     name: "Submarine Sandwich",
     price: 300,
     addItem: false,
   },
   {
     id: 3,
-    img: "../../assets/pizza.svg",
+    img: "/assets/pizza.svg",
     name: "pizza Slices",
     price: 300,
     addItem: false,
